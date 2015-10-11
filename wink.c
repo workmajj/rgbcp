ca53f3067b176fa91a8c94a370c6c523bbd91fed
John J. Workman <johnjworkman@gmail.com>
2015-10-11 13:44:16 -0400

    Quick clean-up

--- wink.c
+++ wink.c
@@ -21,37 +21,38 @@ enum {X, R, G, B}; // 0rgb format
 int main(int argc, char *argv[])
 {
     uint32_t frame = 0;
-    uint32_t bytes = 0; // per frame
+
+    uint32_t byte_count = 0;
+    uint32_t byte_sum = 0;
 
     uint8_t curr;
-
-    uint32_t sum = 0;
 
     while (1) {
         curr = fgetc(stdin);
 
-        switch (bytes % 4) {
+        switch (byte_count % 4) {
         case X:
             assert(curr == 255); // sync
             break;
         case R:
         case G:
         case B:
-            sum += curr;
+            byte_sum += curr;
             break;
         default:
             assert(0 && "not reached");
         }
 
-        bytes++;
+        byte_count++;
 
-        if (bytes == VID_PX_W * VID_PX_H * 4) {
-            printf("frame: %d, bytes: %d, sum: %d\n", frame + 1, bytes, sum);
+        if (byte_count == VID_PX_W * VID_PX_H * 4) {
+            printf("frame: %d\tbyte_count: %d\tbyte_sum: %d\n",
+                frame + 1, byte_count, byte_sum);
 
             frame++;
 
-            bytes = 0;
-            sum = 0;
+            byte_count = 0;
+            byte_sum = 0;
         }
     }
 
