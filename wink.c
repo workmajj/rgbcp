9d6a391782358d71697274af5330e324cada76bc
John J. Workman <johnjworkman@gmail.com>
2015-10-12 00:08:12 -0400

    Calculate average brightness over window of frames

--- wink.c
+++ wink.c
@@ -10,53 +10,71 @@ ffmpeg \
 */
 
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define VID_PX_W 320
 #define VID_PX_H 240
 
-#define FRAME_PX VID_PX_W * VID_PX_H
-#define FRAME_BYTES_TOT FRAME_PX * 4
-#define FRAME_BYTES_RGB FRAME_PX * 3
+#define FRAMES_PER_BIT 15
 
 enum {X, R, G, B}; // 0rgb format
 
+void draw(unsigned long *buf, const unsigned long frame)
+{
+    unsigned long sum = 0;
+
+    for (int i = 0; i < FRAMES_PER_BIT; i++) {
+        sum += buf[i];
+    }
+
+    float bright = (sum / (float)FRAMES_PER_BIT) /
+        (VID_PX_W * VID_PX_H * 3 * 255);
+
+    printf("bit frame: %lu\tbrightness: %.2f%%\n",
+        frame / FRAMES_PER_BIT, bright * 100);
+}
+
 int main(int argc, char *argv[])
 {
-    uint32_t frame = 0;
+    unsigned long buf[FRAMES_PER_BIT] = {0};
 
-    uint32_t byte_count = 0;
-    uint32_t byte_sum = 0;
+    unsigned long vid_bytes = 0;
+    unsigned long vid_frame = 0;
 
-    uint8_t curr;
+    unsigned char c;
 
     while (1) {
-        curr = fgetc(stdin);
+        c = fgetc(stdin);
 
-        switch (byte_count % 4) {
+        switch (vid_bytes % 4) {
         case X:
-            assert(curr == 255); // sync
+            assert(c == 255 && "not synced");
             break;
-        case R: // fall through
-        case G: // fall through
+        case R: // fallthru
+        case G: // fallthru
         case B:
-            byte_sum += curr;
+            buf[vid_frame % FRAMES_PER_BIT] += c;
             break;
         default:
             assert(0 && "not reached");
         }
 
-        byte_count++;
+        vid_bytes++;
+        assert(vid_bytes > 0 && "overflow");
 
-        if (byte_count == FRAME_BYTES_TOT) {
-            printf("frame: %d\tbrightness: %.2f%%\n",
-                frame + 1, ((float)byte_sum / (FRAME_BYTES_RGB * 255)) * 100);
+        if (vid_bytes == VID_PX_W * VID_PX_H * 4) {
+            vid_bytes = 0;
 
-            frame++;
+            vid_frame++;
+            assert(vid_frame > 0 && "overflow");
 
-            byte_count = 0;
-            byte_sum = 0;
+            if (vid_frame % FRAMES_PER_BIT == 0) {
+                draw(buf, vid_frame);
+                memset(buf, 0, sizeof(buf));
+            }
         }
     }
 
