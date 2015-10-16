20d50ec0c6c09249d20d36b952235a39f9700b2f
John J. Workman <johnjworkman@gmail.com>
2015-10-16 01:39:49 -0400

    Break into frame_get() and frame_get_avg() helper

--- src/rx.c
+++ src/rx.c
@@ -10,22 +10,23 @@ ffmpeg \
 */
 
 #include <assert.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FRAME_PX_W 320
-#define FRAME_PX_H 240
+#define FRAME_W 320
+#define FRAME_H 240
 
 typedef struct {
     uint8_t r;
     uint8_t g;
     uint8_t b;
-} Pixel;
+} FramePixel;
 
-Pixel frame_get_avg()
+typedef enum {X, R, G, B} FrameColor;
+
+FramePixel frame_get_avg()
 {
-    Pixel avg;
+    FramePixel avg;
 
     unsigned long sum_r = 0;
     unsigned long sum_g = 0;
@@ -34,7 +35,7 @@ Pixel frame_get_avg()
     unsigned long bytes = 0;
     unsigned char c;
 
-    while (bytes < FRAME_PX_W * FRAME_PX_H * 4) { // 0rgb format
+    while (bytes < FRAME_W * FRAME_H * 4) { // 0rgb format
         c = fgetc(stdin);
 
         switch (bytes % 4) {
@@ -58,155 +59,98 @@ Pixel frame_get_avg()
         assert(bytes > 0 && "overflow");
     }
 
-    // FIXME: round instead of truncating
-    avg.r = sum_r / (FRAME_PX_W * FRAME_PX_H);
-    avg.g = sum_g / (FRAME_PX_W * FRAME_PX_H);
-    avg.b = sum_b / (FRAME_PX_W * FRAME_PX_H);
+    // FIXME: round versus truncating
+    avg.r = sum_r / (FRAME_W * FRAME_H);
+    avg.g = sum_g / (FRAME_W * FRAME_H);
+    avg.b = sum_b / (FRAME_W * FRAME_H);
 
     return avg;
 }
 
-typedef enum {X, R, G, B} FrameColor;
+FrameColor frame_get(FrameColor prev[])
+{
+    assert(prev != NULL);
+
+    FrameColor curr;
+    FrameColor note;
+
+    FramePixel avg = frame_get_avg();
+
+    printf("\tr: %d\tg: %d\tb: %d", avg.r, avg.g, avg.b);
+
+    if (avg.r < 256 * 0.25 && avg.g < 256 * 0.25 && avg.b < 256 * 0.25) {
+        curr = X;
+        note = X;
+    }
+    else if (avg.r > avg.b && avg.r > avg.g) {
+        if (prev[2] == R && prev[1] == G && prev[0] == G) {
+            curr = R;
+            note = R;
+        }
+        else {
+            curr = R;
+            note = X;
+        }
+    }
+    else if (avg.g > avg.r && avg.g > avg.b) {
+        if (prev[2] == G &&
+            (prev[1] == R || prev[1] == B) &&
+            (prev[0] == R || prev[0] == B)) {
+                curr = G;
+                note = G;
+        }
+        else {
+            curr = G;
+            note = X;
+        }
+    }
+    else if (avg.b > avg.r && avg.b > avg.g) {
+        if (prev[2] == B && prev[1] == G && prev[0] == G) {
+            curr = B;
+            note = B;
+        }
+        else {
+            curr = B;
+            note = X;
+        }
+    }
+    else {
+        curr = X;
+        note = X;
+    }
+
+    prev[0] = prev[1];
+    prev[1] = prev[2];
+    prev[2] = note;
+
+    return curr;
+}
 
 int main()
 {
     unsigned long frame = 0;
 
-    /*
-    Pixel curr;
-    Pixel last;
+    FrameColor prev[3];
 
     while (1) {
-        curr = frame_get_avg();
+        printf("frame: %lu", frame + 1);
 
-        printf("frame: %lu\tr: %d\tg: %d\tb: %d\n", frame + 1,
-            avg.r, avg.g, avg.b);
-
-        frame++;
-        assert(frame > 0 && "overflow");
-
-        last = curr;
-    }
-    */
-
-    Pixel avg;
-
-    FrameColor curr;
-    FrameColor last[3];
-
-    /*
-    R R R => ?
-    B B B => ?
-
-    R R B => B <=
-    B B R => R <=
-
-    B R R => ?
-    R B B => ?
-
-    R B R => ?
-    B R B => ?
-    */
-
-    while (1) {
-        avg = frame_get_avg();
-
-        printf("frame: %lu\tr: %d\tg: %d\tb: %d\t", frame + 1,
-            avg.r, avg.g, avg.b);
-
-        /*
-        if (avg.r < 255 * 0.25 && avg.g < 255 * 0.25 && avg.b < 255 * 0.25) {
-            printf("\t--\t--\n");
-            curr = X;
-        }
-        else if (avg.r > 255 * 0.75 && avg.b < 255 * 0.25) {
-            curr = R;
-            printf("\tR\t1\n");
-        }
-        else if (avg.b > 255 * 0.75 && avg.r < 255 * 0.25) {
-            curr = B;
-            printf("\tB\t0\n");
-        }
-        else if (avg.r > avg.b) {
-            if (last[2] == R && last[1] == B && last[0] == B) {
-                curr = R;
-                printf("\tR\t1\n");
-            }
-            else {
-                curr = X;
-                printf("\tR?\t1?\n");
-            }
-        }
-        else if (avg.b > avg.r) {
-            if (last[2] == B && last[1] == R && last[0] == R) {
-                curr = B;
-                printf("\tB\t0\n");
-            }
-            else {
-                curr = X;
-                printf("\tB?\t0?\n");
-            }
-        }
-        else {
-            printf("\t??\t??\n");
-            curr = X;
-        }
-        */
-
-        if (avg.r < 255 * 0.25 && avg.g < 255 * 0.25 && avg.b < 255 * 0.25) {
+        switch (frame_get(prev)) {
+        case X:
             printf("\t-\n");
-            curr = X;
-        }
-        else if (avg.r > 255 * 0.75 && avg.g < 255 * 0.25 && avg.b < 255 * 0.25) {
-            curr = R;
+            break;
+        case R:
             printf("\tR\t0\n");
-        }
-        else if (avg.r < 255 * 0.25 && avg.g > 255 * 0.75 && avg.b < 255 * 0.25) {
-            curr = R;
+            break;
+        case G:
             printf("\tG\n");
-        }
-        else if (avg.r < 255 * 0.25 && avg.g < 255 * 0.25 && avg.b > 255 * 0.75) {
-            curr = R;
+            break;
+        case B:
             printf("\tB\t1\n");
+            break;
+        default:
+            assert(0 && "not reached");
         }
-        else if (avg.r > avg.b && avg.r > avg.g) {
-            if (last[2] == R && last[1] != R && last[0] != R) {
-                curr = R;
-                printf("\tR\t0\n");
-            }
-            else {
-                curr = X;
-                printf("\tR?\t0\n");
-            }
-        }
-        else if (avg.g > avg.r && avg.g > avg.b) {
-            if (last[2] == G && last[1] != G && last[0] != G) {
-                curr = G;
-                printf("\tG\n");
-            }
-            else {
-                curr = X;
-                printf("\tG?\n");
-            }
-        }
-        else if (avg.b > avg.r && avg.b > avg.g) {
-            if (last[2] == B && last[1] != B && last[0] != B) {
-                curr = B;
-                printf("\tB\t1\n");
-            }
-            else {
-                curr = X;
-                printf("\tB?\t1\n");
-            }
-        }
-        else {
-            printf("\t??\n");
-            curr = X;
-        }
-
-        last[0] = last[1];
-        last[1] = last[2];
-        last[2] = curr;
 
         frame++;
         assert(frame > 0 && "overflow");
